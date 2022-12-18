3fbd9af2614e4c54af4032e5ddaa471c488efcfc
Alex <ashkodin@mail.ru>
2022-12-18 13:55:00 +0500

    add seminar9

---
+++ C#/seminar9/task2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в 
+// промежутке от 1 до N при помощи рекурсии.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+void PrintNumbers(int number)
+{
+    if (number == 0)
+    {
+        return;
+    }
+
+    System.Console.Write($"{number}; ");
+    PrintNumbers(number - 1);
+    System.Console.Write($"{number}; ");
+}
+
+int number = 4; //Prompt("Введите число");
+PrintNumbers(number);
+void PrintNumbers(int numberStart, int numberFinish)
+{
+    if (numberStart == numberFinish)
+    {
+        return;
+    }
+    System.Console.Write($"{numberStart}; ");
+    PrintNumbers(numberStart + 1,numberFinish);
+}
+
+int numberStart = 4;
+int numberFinish=9;
+ //Prompt("Введите число");
+PrintNumbers(numberStart,numberFinish);
+
+
