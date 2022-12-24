b1a251a892cb50980faf759313b9b9daa95ab160
Alex <ashkodin@mail.ru>
2022-12-24 17:40:34 +0500

    add homework9

--- C#/homework9/task2/Program.cs
+++ C#/homework9/task2/Program.cs
@@ -2,3 +2,19 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+int Prompt(string message)
+{
+    Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int PlusNatural(int M, int N)
+{
+    int sum = 0;
+    sum = sum + M;
+    if (M < N) return M + PlusNatural(M + 1, N);
+    return sum;
+}
+
+Console.WriteLine($"{PlusNatural(Prompt("Введите M"), Prompt("Введите N"))}");
